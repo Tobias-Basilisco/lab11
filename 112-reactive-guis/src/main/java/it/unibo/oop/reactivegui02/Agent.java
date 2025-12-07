3c231af75afdc1a2fd24ca812b0bea4626429689
Tobias-Basilisco <tobiasbasilisco@gmail.com>
2025-12-07 15:47:25 +0100

    ConcurrentGUI02 with exec and separated Agent but no additional functionalities

---
+++ 112-reactive-guis/src/main/java/it/unibo/oop/reactivegui02/Agent.java
@@ -0,0 +1,43 @@
+package it.unibo.oop.reactivegui02;
+
+import java.lang.reflect.InvocationTargetException;
+
+import javax.swing.JLabel;
+import javax.swing.SwingUtilities;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Agent implements Runnable {
+
+    private volatile boolean stop = false;
+    private final JLabel display;
+    private int counter = 0;
+    private static final Logger LOGGER = LoggerFactory.getLogger(ConcurrentGUI.class);
+
+    public Agent(JLabel display) {
+        this.display = display;
+    }
+
+    @Override
+    public void run() {
+        while (!this.stop) {
+            try {
+                // The EDT doesn't access `counter` anymore, it doesn't need to be volatile
+                final var nextText = Integer.toString(this.counter);
+                SwingUtilities.invokeAndWait(() -> display.setText(nextText));
+                this.counter++;
+                Thread.sleep(100);
+            } catch (InvocationTargetException | InterruptedException ex) {
+                LOGGER.error(ex.getMessage(), ex);
+            }
+        }
+    }
+
+    /**
+     * External command to stop counting.
+     */
+    public void stopCounting() {
+        stop = true;
+    }
+}
--- 112-reactive-guis/src/main/java/it/unibo/oop/reactivegui02/ConcurrentGUI.java
+++ 112-reactive-guis/src/main/java/it/unibo/oop/reactivegui02/ConcurrentGUI.java
@@ -1,7 +1,18 @@
 package it.unibo.oop.reactivegui02;
 
-import javax.swing.JFrame;
+import it.unibo.oop.JFrameUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.Serial;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 
 /**
  * Second example of reactive GUI.
@@ -10,4 +21,36 @@ public final class ConcurrentGUI extends JFrame {
 
     @Serial
     private static final long serialVersionUID = 1L;
+    private static final Logger LOGGER = LoggerFactory.getLogger(ConcurrentGUI.class);
+    private final JLabel display = new JLabel();
+
+    private final Agent agent = new Agent(display);
+    private final int threadPoolSize = Runtime.getRuntime().availableProcessors();
+    private final ExecutorService exec = Executors.newFixedThreadPool(threadPoolSize);		
+
+
+    /**
+     * Builds a new CGUI.
+     */
+    public ConcurrentGUI() {
+        super();
+        JFrameUtil.dimensionJFrame(this);
+        final JPanel panel = new JPanel();
+        panel.add(display);
+        final JButton stop = new JButton("stop");
+        panel.add(stop);
+        this.getContentPane().add(panel);
+        this.setVisible(true);
+
+        /*
+         * thread management should be left to
+         * java.util.concurrent.ExecutorService
+         */
+        exec.execute(agent);
+        /*
+         * Register a listener that stops it
+         */
+        stop.addActionListener(e -> agent.stopCounting());
+    }
+
 }
