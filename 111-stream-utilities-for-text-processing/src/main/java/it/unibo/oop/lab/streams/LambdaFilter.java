cf13cf45a0cc21eac1a33b9ed55bfe29ece02529
Tobias-Basilisco <tobiasbasilisco@gmail.com>
2025-12-07 12:10:49 +0100

    TO_LOWER

--- 111-stream-utilities-for-text-processing/src/main/java/it/unibo/oop/lab/streams/LambdaFilter.java
+++ 111-stream-utilities-for-text-processing/src/main/java/it/unibo/oop/lab/streams/LambdaFilter.java
@@ -41,7 +41,8 @@ public final class LambdaFilter extends JFrame {
         /**
          * Commands.
          */
-        IDENTITY("No modifications", Function.identity());
+        IDENTITY("No modifications", Function.identity()),
+        TO_LOWER("Convert to lowercase", s -> s.toLowerCase());
 
         private final String commandName;
         private final Function<String, String> fun;
